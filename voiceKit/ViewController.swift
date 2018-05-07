5710ec13c555a934b769a1ba02e8d6062b0608b3
nesterov <faktorealchik@gmail.com>
2018-05-07 17:37:07 +0300

    Initial Commit

---
+++ voiceKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voiceKit
+//
+//  Created by Alexandr Nesterov on 5/7/18.
+//  Copyright © 2018 Alexandr Nesterov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
